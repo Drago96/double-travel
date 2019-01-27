7cecd1795cbe844e9af6579f8594740fdae92b5f
Drago96 <dragproychev@gmail.com>
2019-01-27 20:47:27 +0200

    Added jumbotron to main page

---
+++ Views/Home/index.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="/Assets/styles/home/index.css">
+
+<section class="jumbotron">
+  <section class="welcome-message">
+    <h1>Welcome to Double Travel!</h1>
+    <p>The best place to plan your upcoming journeys.</p>
+  </section>
+</section>
